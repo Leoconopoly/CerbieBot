0291f788ca309c318310eb5956d0b36c21f9454d
Leoconopoly <98611409+Leoconopoly@users.noreply.github.com>
2023-08-11 01:18:54 +0100

    updated commands

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
 
 # Define the intents of the bot
 intents = discord.Intents.default()
@@ -14,7 +15,7 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 AUTHORIZED_USER_IDS = os.getenv('AUTHORIZED_USER_IDS').split(',')
 
 # Initialize the bot with the specified intents
-bot = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -30,18 +31,37 @@ async def on_message(message):
     if str(message.author.id) not in AUTHORIZED_USER_IDS:
         return
 
+# Check if the bot was mentioned
+    if bot.user.mentioned_in(message):
+    # Respond to the mention
+        await message.channel.send("You mentioned me!")
+
+    # Strip the mention from the message content
+    content_without_mention = message.content.replace(f"<@!{bot.user.id}>", "").replace(f"<@{bot.user.id}>", "").strip()
+
+    print(f"Content without mention: {content_without_mention}")  # Debug print
+
+    # Process the query based on your existing conditions
     # Greetings
     greetings = ["hello", "hi", "hey"]
-    if message.content.lower() in greetings:
+    if content_without_mention.lower() in greetings:
+        print("Greeting detected")  # Debug print
         await message.channel.send("WOOF! Hi there!")
 
     # Bot's Status
-    elif message.content.lower() == "how are you?":
+    elif content_without_mention.lower() == "how are you?":
+        print("Status query detected")  # Debug print
         await message.channel.send("I'm just a cute doggo bot, but I'm functioning optimally!")
 
     # Bot's Purpose
-    elif message.content.lower() == "what can you do?":
+    elif content_without_mention.lower() == "what can you do?":
+        print("Purpose query detected")  # Debug print
         await message.channel.send("I'm still learning, but I can greet you and answer some basic questions!")
+
+
+
+    # Allow the bot to process commands
+    await bot.process_commands(message)
 
 # Run the bot
 bot.run(DISCORD_TOKEN)
